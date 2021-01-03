c0bed492a71451178157f712743f66ff65e4789d
barisoa029 <76122901+barisoa029@users.noreply.github.com>
2021-01-03 18:22:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,3 +3,21 @@ const client = new Discord.Client();
 const fs = require("fs");
 const logs = require("discord-logs");
 logs(client);
+const ayarlar = require("./ayarlar.json");
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) return console.log(err);
+  console.log(`${files.length} kadar komut bulundu, yükleniyor...`);
+  files.forEach(f => {
+    let cmds = require(`./komutlar/${f}`);
+    console.log(`Komut yükleniyor: ${cmds.help.name}`);
+    client.commands.set(cmds.help.name, cmds);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, cmds.help.name);
+    });
+  })
+})
+
+client.login(ayarlar.token);
